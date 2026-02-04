135be309b1f2c70a5d83de62603c57e6a8c10734
Rashmitha3105 <rashmitha.23cs117@sode-edu.in>
2026-02-04 12:12:05 +0530

    lab1.py

--- lab1.py
+++ lab1.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing
 
 data=fetch_california_housing(as_frame=True)
-print(data.frame.head())
 housing_df=data.frame
 
 numerical_features=housing_df.select_dtypes(include=[np.number]).columns
